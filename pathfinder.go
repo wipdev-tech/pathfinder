9ccfaf1255594720d7aa486126993dabd5cfef6c
wipdev-tech <wipdev.tech@gmail.com>
2023-10-05 23:17:34 +0300

    feat: help command

--- pathfinder.go
+++ pathfinder.go
@@ -6,6 +6,23 @@ import (
 )
 
 func main() {
-    fmt.Println("Pathfinder!")
-    fmt.Println(os.Args)
+    if len(os.Args) < 2 || os.Args[1] == "help" {
+        printHelp()
+    }
+}
+
+func printHelp() {
+    helpLines := []string{
+        "Pathfinder - A simple CLI for managing path variables",
+        "",
+        "Commands:",
+        "    help                     Print this message",
+        "    list                     List paths in the PATH variable",
+        "    add <path> [--persist]   Add <path> to the PATH variable",
+        "                             (--persist modifies .bashrc to persist the new path)",
+    }
+
+    for _, l := range helpLines {
+        fmt.Println(l)
+    }
 }

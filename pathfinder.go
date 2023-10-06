df38af7fd621f245c1917608d09abc734d6f6a52
wipdev-tech <wipdev.tech@gmail.com>
2023-10-06 23:54:16 +0300

    feat: list path

--- pathfinder.go
+++ pathfinder.go
@@ -1,28 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
 )
 
 func main() {
-    if len(os.Args) < 2 || os.Args[1] == "help" {
-        printHelp()
-    }
+	if len(os.Args) < 2 || os.Args[1] == "help" {
+		printHelp()
+		return
+	}
+
+	if os.Args[1] == "list" {
+		sortPaths := len(os.Args) == 3 && os.Args[2] == "--sort"
+		printList(sortPaths)
+		return
+	}
 }
 
 func printHelp() {
-    helpLines := []string{
-        "Pathfinder - A simple CLI for managing path variables",
-        "",
-        "Commands:",
-        "    help                     Print this message",
-        "    list                     List paths in the PATH variable",
-        "    add <path> [--persist]   Add <path> to the PATH variable",
-        "                             (--persist modifies .bashrc to persist the new path)",
-    }
+	helpLines := []string{
+		"Pathfinder - A simple CLI for managing path variables",
+		"",
+		"Commands:",
+		"    help                     Print this message",
+		"    list [--sort]            List paths in the PATH variable",
+		"                             (--sort orders them alphabettically)",
+		"    add <path> [--persist]   Add <path> to the PATH variable",
+		"                             (--persist modifies .bashrc to persist the new path)",
+	}
 
-    for _, l := range helpLines {
-        fmt.Println(l)
-    }
+	for _, l := range helpLines {
+		fmt.Println(l)
+	}
+}
+
+func printList(sort bool) {
+	paths := strings.Split(os.Getenv("PATH"), ":")
+
+	if sort {
+		slices.Sort(paths)
+	}
+
+	for _, p := range paths {
+		fmt.Println(p)
+	}
 }

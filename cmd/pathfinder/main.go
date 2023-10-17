e09870849eb41e59c076b0907f9818944a29b065
wipdev-tech <wipdev.tech@gmail.com>
2023-10-17 16:31:05 +0300

    feat(add): fn that prints .profile

--- cmd/pathfinder/main.go
+++ cmd/pathfinder/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"slices"
@@ -8,12 +9,16 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 || os.Args[1] == "help" {
+	if len(os.Args) < 2 {
 		PrintHelp()
 		return
 	}
 
-	if os.Args[1] == "list" {
+	switch os.Args[1] {
+	case "help":
+		PrintHelp()
+
+	case "list":
 		listArgs := os.Args[2:]
 		sortPaths := slices.Contains(listArgs, "--sort")
 		searchString := ""
@@ -21,16 +26,25 @@ func main() {
 		for i, arg := range listArgs {
 			if arg == "--search" {
 				searchString = listArgs[i+1]
-                break
+				break
 			}
 		}
 
 		PrintList(sortPaths, searchString)
-		return
-	}
 
-    fmt.Println("Pathfinder: unknown command")
-    fmt.Println("Run 'pathfinder help' for info")
+	case "add":
+		if len(os.Args) < 3 {
+			fmt.Println("Pathfinder: no path provided")
+			return
+		}
+
+        checkDotProfile()
+		// addPath(os.Args[2])
+
+	default:
+		fmt.Println("Pathfinder: unknown command")
+		fmt.Println("Run 'pathfinder help' for info")
+	}
 }
 
 func PrintHelp() {
@@ -40,7 +54,7 @@ func PrintHelp() {
 		"Commands:",
 		"    help                     Print this message",
 		"    list [--sort]            List paths in the PATH variable",
-		"                             (--sort orders them alphabettically)",
+		"         [--search string]",
 		"    add <path> [--persist]   Add <path> to the PATH variable",
 		"                             (--persist modifies .bashrc to persist the new path)",
 	}
@@ -49,7 +63,6 @@ func PrintHelp() {
 		fmt.Println(l)
 	}
 }
-
 
 func PrintList(sort bool, searchStr string) {
 	paths := strings.Split(os.Getenv("PATH"), ":")
@@ -64,3 +77,38 @@ func PrintList(sort bool, searchStr string) {
 		}
 	}
 }
+
+func checkDotProfile() {
+	fr, err := os.OpenFile(
+		os.Getenv("HOME")+"/.profile",
+		os.O_RDONLY, 0644,
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer fr.Close()
+
+	scanner := bufio.NewScanner(fr)
+    fmt.Println("before:")
+	for scanner.Scan() {
+		line := scanner.Text()
+        fmt.Println(line)
+	}
+
+}
+
+// func addPath(newPath string) {
+// 	fw, err := os.OpenFile(
+// 		os.Getenv("HOME")+"/Downloads/hamada.txt",
+// 		os.O_WRONLY|os.O_APPEND, 0644,
+// 	)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	defer fw.Close()
+//
+//     _, err = fw.WriteString("a7lamesa3lek\n")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// }
